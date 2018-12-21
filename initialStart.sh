5f59b7696d683e7c2e1e68e080e152d51adc3719
Chainoid Team <chainoid@gmx.com>
2018-12-21 19:26:24 +0200

    Tested with four nodes is production mode.

--- initialStart.sh
+++ initialStart.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Exit on first error.
-set -ev
+set -e
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
@@ -13,32 +13,19 @@ export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
 
 docker-compose -f ./network-config/docker-compose-kafka.yml down
+
+sleep 1
+
 docker-compose -f ./network-config/docker-compose-cli.yml down
 #docker-compose -f ./network-config/docker-compose-couchdb.yml down
 
 docker-compose -f ./network-config/docker-compose-kafka.yml up -d
+
+sleep 1
+
 docker-compose -f ./network-config/docker-compose-cli.yml up -d
 #docker-compose -f ./network-config/docker-compose-couchdb.yml up -d
 
-# wait for Hyperledger Fabric to start
-# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=15
-echo ${FABRIC_START_TIMEOUT}
-sleep ${FABRIC_START_TIMEOUT}
-
-# # # Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer0.example.com:7050 -c posta-channel -f /var/hyperledger/configs/channel.tx
-# # # Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b posta-channel.block
-
-# # # Join peer1.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" peer0.org1.example.com peer channel join -b posta-channel.block
-
-# # # Join peer2.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer2.org1.example.com:7051" peer0.org1.example.com peer channel join -b posta-channel.block
-
-# # # Join peer3.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer3.org1.example.com:7051" peer0.org1.example.com peer channel join -b posta-channel.block
 
 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
--- install.sh
+++ install.sh
@@ -1,6 +1,35 @@
-# to be executed on start
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error.
+set -e
 
 starttime=$(date +%s)
+
+# # # Create the channel
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer0.example.com:7050 -c posta-channel -f /var/hyperledger/configs/channel.tx
+# # # Join peer0.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b posta-channel.block
+
+# # # Join peer1.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" peer0.org1.example.com peer channel join -b posta-channel.block
+
+# # # Join peer2.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer2.org1.example.com:7051" peer0.org1.example.com peer channel join -b posta-channel.block
+
+# # # Join peer3.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/var/hyperledger/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer3.org1.example.com:7051" peer0.org1.example.com peer channel join -b posta-channel.block
+
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=12
+echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
 
 # Install application to the first peer
 docker exec -it cli peer chaincode install -n postap -p github.com/chaincode -v v0.1 --peerAddresses peer0.org1.example.com:7051
@@ -22,9 +51,9 @@ docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C p
 
 docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C posta-channel -n postap github.com/chaincode -v v0.1 --peerAddresses peer1.org1.example.com:7051 -c '{"Args":["initLedger"]}'
 
-docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C posta-channel -n postap github.com/chaincode -v v0.1 --peerAddresses peer2.org1.example.com:7051 -c '{"Args":["initLedger"]}'
+#docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C posta-channel -n postap github.com/chaincode -v v0.1 --peerAddresses peer2.org1.example.com:7051 -c '{"Args":["initLedger"]}'
 
-docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C posta-channel -n postap github.com/chaincode -v v0.1 --peerAddresses peer3.org1.example.com:7051 -c '{"Args":["initLedger"]}'
+#docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C posta-channel -n postap github.com/chaincode -v v0.1 --peerAddresses peer3.org1.example.com:7051 -c '{"Args":["initLedger"]}'
 
 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
